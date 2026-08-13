145ec4706945f70b3f863a36d6183cf198b9311d
Yazhini01-k <yazhiniarasan28@gmail.com>
2026-08-13 21:25:42 +0530

    output:azbycxdwevfugthsirjqkplomnnm

---
+++ Array/src/arrayQuestion/alphabet.java
@@ -0,0 +1,11 @@
+package arrayQuestion;
+
+public class alphabet {
+	public static void main(String[] args) {
+		for(int i =0; i<=13; i++) {
+			System.out.print((char) ('a' + i));
+			System.out.print((char) ('z' - i));
+			
+		}
+	}
+}
